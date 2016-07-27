a579e4fed108752e63f6843e845d2700e19c7497
andressamaf <aasf93@hotmail.es>
2016-07-26 21:01:45 -0500

    Vistas y Servicios de actor y director
    Completo creo :dancers:

--- src/ec/edu/epn/getmovie/model/service/actor/ServiceActor.java
+++ src/ec/edu/epn/getmovie/model/service/actor/ServiceActor.java
@@ -1,29 +1,73 @@
 package ec.edu.epn.getmovie.model.service.actor;
 import java.util.Collection;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
 import ec.edu.epn.getmovie.model.Actor;
+
+@Path("actor")
+@Produces("application/json")
 public class ServiceActor {
-	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("GetMovieJPA");
-	public Actor buscarActor(int idActor) {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		Actor actor = em.find(Actor.class, idActor);
-		em.getTransaction().commit();
-		em.close();
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("GetMovieREST");
+	@GET
+	@Path("buscar")
+	public Actor buscarActor(@QueryParam("id")int idActor) {
+		String mensaje="";
+		Actor actor = new Actor();
+		try{
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			actor = em.find(Actor.class, idActor);
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+			mensaje="Usuario encontrado";
+		}catch(Exception e){
+			e.printStackTrace();
+			mensaje="error buscando";
+		}
 		return actor;
 	}
-	public void crearActor(Actor actor) {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(actor);
-		em.getTransaction().commit();
-		em.close();
+	@POST
+	@Path("crear")
+	public String crearActor(
+			@QueryParam("nombre")String nombre,
+			@QueryParam("genero")String genero,
+			@QueryParam("nacimiento")String nacimiento,
+			@QueryParam("oscars")int oscar
+			) {
+		try{
+			EntityManager em = emf.createEntityManager();
+			Actor actor = new Actor();
+			actor.setNombreactor(nombre);
+			actor.setGenero(genero);
+			actor.setOscars(oscar);
+			actor.setNacimiento(nacimiento);
+			em.getTransaction().begin();
+			em.persist(actor);
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return "Actor creado";
 	}
+	@GET
+	@Path("listar")
 	@SuppressWarnings("unchecked")
-	public Collection<Actor> listarActor(String nombreActor) {
+	public Collection<Actor> listarActor(@QueryParam("nombre")String nombreActor) {
 		EntityManager em = emf.createEntityManager();
 		Query q = em.createNamedQuery("Actor.findByNombre");
 		q.setParameter("nombre", "%" + nombreActor + "%");
@@ -31,23 +75,49 @@ public class ServiceActor {
 		em.close();	
 		return listaActor;
 	}
-	public void eliminarActor(int idActor) {
-		EntityManager em = emf.createEntityManager();
-		Actor actor = em.find(Actor.class, idActor);
-		em.getTransaction().begin();
-		em.remove(actor);
-		em.getTransaction().commit();
-		em.close();
+	@DELETE
+	@Path("eliminar")
+	public String eliminarActor(@QueryParam("id")int idActor) {
+		try{
+			EntityManager em = emf.createEntityManager();
+			Actor actor = em.find(Actor.class, idActor);
+			em.getTransaction().begin();
+			em.remove(actor);
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return "usuario eliminado";
 	}
-	public void modificarActor(Actor actorPost){
-		EntityManager em = emf.createEntityManager();
-		Actor actorGet = em.find(Actor.class, actorPost.getIdactor());
-		em.getTransaction().begin();
-		actorGet.setNombreactor(actorPost.getNombreactor());
-		actorGet.setGenero(actorPost.getGenero());
-		actorGet.setOscars(actorPost.getOscars());
-		actorGet.setNacimiento(actorPost.getNacimiento());
-		em.getTransaction().commit();
-		em.close();
+	@PUT
+	@Path("modificar")
+	public String modificarActor(
+			@QueryParam("nombre")String nombre,
+			@QueryParam("genero")String genero,
+			@QueryParam("nacimiento")String nacimiento,
+			@QueryParam("oscars")int oscar,
+			@QueryParam("id")int id
+			){
+		String mensaje="";
+		try{
+			EntityManager em = emf.createEntityManager();
+			Actor actorGet = em.find(Actor.class, id);
+			em.getTransaction().begin();
+			actorGet.setNombreactor(nombre);
+			actorGet.setGenero(genero);
+			actorGet.setOscars(oscar);
+			actorGet.setNacimiento(nacimiento);
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+			mensaje = "usuario modificado";
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			mensaje = "error modificando";
+		}
+		return mensaje;
 	}
 }
--- src/ec/edu/epn/getmovie/model/service/director/ServiceDirector.java
+++ src/ec/edu/epn/getmovie/model/service/director/ServiceDirector.java
@@ -1,53 +1,114 @@
 package ec.edu.epn.getmovie.model.service.director;
 import java.util.Collection;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
 import ec.edu.epn.getmovie.model.Director;
+@Path("Director")
+@Produces("application/json")
 public class ServiceDirector {
-	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("GetMovieJPA");
-	public Director buscarDirector(int idDirector) {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		Director director = em.find(Director.class, idDirector);
-		em.getTransaction().commit();
-		em.close();
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("GetMovieREST");
+	@GET
+	@Path("buscar")
+	public Director buscarDirector(@QueryParam("id")int idDirector) {
+		Director director = new Director();
+		try{
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			director = em.find(Director.class, idDirector);
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		return director;
 	}
-	public void crearDirector(Director director) {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(director);
-		em.getTransaction().commit();
-		em.close();
+	@POST
+	@Path("crear")
+	public void crearDirector(
+			@QueryParam("nombre")String nombre,
+			@QueryParam("genero")String genero,
+			@QueryParam("nacimiento")String nacimiento
+			){
+		Director director = new Director();
+		try{
+			director.setGenero(genero);
+			director.setNombredirector(nombre);
+			director.setNacimiento(nacimiento);
+			EntityManager em = emf.createEntityManager();
+			em.getTransaction().begin();
+			em.persist(director);
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
+	@GET
+	@Path("listar")
 	@SuppressWarnings("unchecked")
-	public Collection<Director> listarDirector(String nombreDirector) {
-		EntityManager em = emf.createEntityManager();
-		Query q = em.createNamedQuery("Director.findByNombre");
-		q.setParameter("nombre", "%" + nombreDirector + "%");
-		Collection<Director> listaDirector = q.getResultList();
-		em.close();	
+	public Collection<Director> listarDirector(@QueryParam("nombre")String nombreDirector) {
+		Collection<Director> listaDirector = null;
+		try{
+			EntityManager em = emf.createEntityManager();
+			Query q = em.createNamedQuery("Director.findByNombre");
+			q.setParameter("nombre", "%" + nombreDirector + "%");
+			listaDirector = q.getResultList();
+			em.close();
+			emf.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		return listaDirector;
 	}
-	public void eliminarDirector(int idDirector) {
-		EntityManager em = emf.createEntityManager();
-		Director director = em.find(Director.class, idDirector);
-		em.getTransaction().begin();	
-		em.remove(director);
-		em.getTransaction().commit();
-		em.close();
+	@DELETE
+	@Path("eliminar")
+	public void eliminarDirector(@QueryParam("id")int idDirector) {
+		try{
+			EntityManager em = emf.createEntityManager();
+			Director director = em.find(Director.class, idDirector);
+			em.getTransaction().begin();	
+			em.remove(director);
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}	
 	}
-	public void modificarDirector(Director directorDespues){
-		EntityManager em = emf.createEntityManager();
-		Director directorAntes = em.find(Director.class, directorDespues.getIddirector());
-		em.getTransaction().begin();
-		directorAntes.setNombredirector(directorDespues.getNombredirector());
-		directorAntes.setGenero(directorDespues.getGenero());
-		directorAntes.setNacimiento(directorDespues.getNacimiento());
-		directorAntes.setFotodirector(directorDespues.getFotodirector());
-		em.getTransaction().commit();
-		em.close();
+	@PUT
+	@Path("modificar")
+	public void modificarDirector(
+			@QueryParam("id")int id,
+			@QueryParam("nombre")String nombre,
+			@QueryParam("genero")String genero,
+			@QueryParam("nacimiento")String nacimiento
+			){
+		
+		try{
+			EntityManager em = emf.createEntityManager();
+			Director director = em.find(Director.class, id);
+			em.getTransaction().begin();
+			director.setNombredirector(nombre);
+			director.setGenero(genero);
+			director.setNacimiento(nacimiento);
+			em.getTransaction().commit();
+			em.close();
+			emf.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 }
